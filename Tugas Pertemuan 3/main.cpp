3ad86aa9137d6a57aa95f3c679ab13aed92873b1
FendyAlamsyah28 <112867259+FendyAlamsyah28@users.noreply.github.com>
2022-10-02 15:36:55 +0700

    Add files via upload
    TUGAS PERTEMUAN 3

---
+++ Tugas Pertemuan 3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Fendy Alamsyah" << endl;
+
+    int u = 18;
+    //bilangan bulat
+    float t = 172.3;
+    //bilangan pecahan
+    char b = 'L';
+    // 1 huruf
+    string ub = "xxl";
+    //lebih dari satu huruf
+    cout << "umur saya: "<< u;
+
+    cout << "tinggi saya: "<< t;
+
+    cout << "ukuran baju: "<< b;
+
+    cout << "ukuran baju: "<< ub;
+    cout << endl;
+
+
+    return 0;
+}
