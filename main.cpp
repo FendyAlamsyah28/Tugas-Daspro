af7d420b46fcc2fbac99c4890aa2b346b57bcb7e
FendyAlamsyah28 <112867259+FendyAlamsyah28@users.noreply.github.com>
2022-10-02 15:31:00 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Fendy Alamsyah" << endl;
+    cout << "NIM : A11.2022.14123" << endl;
+    cout << "Alamat : Jln.Srikandi No.185" << endl;
+    cout << "Asal Sekolah : SMAN 2 Pekalongan" << endl;
+    return 0;
+}
